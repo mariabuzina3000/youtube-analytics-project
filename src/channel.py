8bce3816ce62428c255e45e31d3be819f7ab6289
maria buzina <buzinanm@yandex.ru>
2023-06-25 00:23:19 +0500

    homework_2

--- src/channel.py
+++ src/channel.py
@@ -1,5 +1,6 @@
 import os
 from googleapiclient.discovery import build
+import json
 
 api_key = os.getenv('YT_API_KEY')
 
@@ -9,12 +10,36 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        self.object = self.get_service()
+        self.channel = self.object.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = self.channel['items'][0]['snippet']['thumbnails']['default']['url']
+        self.subscriberCount = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.viewCount = self.channel['items'][0]['statistics']['viewCount']
+
+
+    @classmethod
+    def get_service(cls):
+        return build('youtube', 'v3', developerKey=api_key)
+
 
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        youtube = build('youtube', 'v3', developerKey=api_key)
 
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(channel)
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+    def to_json(self, filename):
+        channel_info = {"title": self.title,
+                        "channel_id": self.channel_id,
+                        "description": self.description,
+                        "url": self.url,
+                        "subscriberCount": self.subscriberCount,
+                        "video_count": self.video_count,
+                        "viewCount": self.viewCount}
+        with open(filename, 'w', encoding='utf-8') as file:
+            json.dump(channel_info, file, indent=4, ensure_ascii=False)
+
 
